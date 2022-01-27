810decbc1a0d77907f09136838b6024c2ee37d33
Warxwell <senhorpompas@gmail.com>
2022-01-26 21:10:14 -0300

    Creacion de modulos propios y de routes

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+
+//modulos
+import { sharedModule } from '../shared/shared.module';
+
+//rutas
+import { PAGES_ROUTES } from './pages.routes';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+import { ProgressComponent } from './progress/progress.component';
+
+
+
+@NgModule ({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        sharedModule,
+        PAGES_ROUTES
+    ]
+})
+export class pagesModule { }
