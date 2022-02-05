49b2dc6d1e774fa51bb311dafa5975590934f3a2
Warxwell <senhorpompas@gmail.com>
2022-02-05 19:00:06 -0300

    account setting component

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from "@angular/router";
 
+
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { Graficas1Component } from "./graficas1/graficas1.component";
 import { PagesComponent } from "./pages.component";
 import { ProgressComponent } from "./progress/progress.component";
+import { AccountSettingsComponent } from "./account-settings/account-settings.component";
 
 
 
@@ -15,6 +17,7 @@ const pagesRoutes: Routes = [
             {path:'dashboard', component: DashboardComponent},
             {path:'progress', component: ProgressComponent},
             {path:'graficas1', component: Graficas1Component},
+            {path:'account-settings', component: AccountSettingsComponent},
             {path:'', redirectTo: '/dashboard', pathMatch: 'full'},
         ]
     }
